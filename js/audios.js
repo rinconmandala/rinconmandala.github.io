4b3bb1ae673384b61a31492b83f01bea199e787b
rinconmandala <91046754+rinconmandala@users.noreply.github.com>
2021-10-12 11:52:00 +0200

    Add files via upload

---
+++ js/audios.js
@@ -0,0 +1,97 @@
+//Audio 11
+var once=new Audio();
+once.src="11.mp3"
+//Audio 12
+var doce=new Audio();
+doce.src="12.mp3"
+//Audio 13
+var trece=new Audio();
+trece.src="13.mp3"
+//Audio 14
+var catorce=new Audio();
+catorce.src="14.mp3"
+//Audio 15
+var quince=new Audio();
+quince.src="15.mp3"
+//Audio Audio 16
+var dieciseis=new Audio();
+dieciseis.src="16.mp3"
+//Audio 17
+var diecisiete=new Audio();
+diecisiete.src="17.mp3"
+//Audio 18    
+var dieciocho=new Audio();
+dieciocho.src="18.mp3"
+//Audio 19
+var dieinueve=new Audio();
+dieinueve.src="19.mp3"
+//Audio 20
+var veinte=new Audio();
+veinte.src="20.mp3"
+//Audio 21
+var veinteuno=new Audio();
+veinteuno.src="21.mp3"
+//Audio 22
+var veintidos=new Audio();
+veintidos.src="22.mp3"
+//Audio 23
+var veintitres=new Audio();
+veintitres.src="23.mp3"
+//Audio 24
+var venticuatro=new Audio();
+venticuatro.src="24.mp3"
+//Audio 25
+var venticinco=new Audio();
+venticinco.src="25.mp3"
+//Audio 26
+var ventiseis=new Audio();
+ventiseis.src="26.mp3"
+//Audio 27
+var ventisiete=new Audio();
+ventisiete.src="27.mp3"
+//Audio 28
+var ventiocho=new Audio();
+ventiocho.src="28.mp3"
+//Audio 29
+var ventinueve=new Audio();
+ventinueve.src="29.mp3"
+//Audio 30
+var treinta=new Audio();
+treinta.src="30.mp3"
+//Audio 31
+var treintauno=new Audio();
+treintauno.src='31.mp3'
+//Audio 32
+var treintidos=new Audio();
+treintidos.src="32.mp3"
+//Audio 33
+var treintares=new Audio();
+treintares.src='33.mp3'
+//Audio 34
+var treintacuatro=new Audio();
+treintacuatro.src="34.mp3"
+
+//Audio 35
+var treintacinco=new Audio();
+treintacinco.src="35.mp3"
+//Audio 36
+var treintaseis=new Audio();
+treintaseis.src="36.mp3"
+//Audio 37
+var treintasiete=new Audio();
+treintasiete.src="37.mp3"
+//Audio 38
+var trentaocho=new Audio();
+trentaocho.src="38.mp3"
+//Audio 39
+var tentranueve=new Audio();
+tentranueve.src="39.mp3"
+//Audio 40
+var cuarenta=new Audio();
+cuarenta.src="40.mp3"
+//Audio 41
+var cuarentauno=new Audio();
+cuarentauno.src="41.mp3"
+//Audio 42
+var cuarentados=new Audio();
+cuarentados.src="42.mp3"
